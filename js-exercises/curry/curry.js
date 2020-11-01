240a49e501fe246b2c6032fd62cfda8656522418
Tahir Ahmad <tahirahmad.in@gmail.com>
2020-11-01 10:45:21 +0530

    Implement curry balancedBraces Bind

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,5 +1,11 @@
-function curry(a) {
-  return a;
+function curry(func) {
+  return function curriedFunc(...args) {
+    if (args.length === func.length) {
+      return func(...args);
+    }
+    return function (...args2) {
+      return curriedFunc(...args.concat(args2));
+    };
+  };
 }
-
 export { curry };
